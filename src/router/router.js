45c330347d327c371613a38ff01d327b47fab3ad
Mosi <mosisu07@gmail.com>
2024-07-22 14:07:52 +0800

    chore: updated components and its path

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
     element: <App />,
   },
 ]);
