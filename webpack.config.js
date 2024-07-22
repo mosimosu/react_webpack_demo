163d22bb8535d8fc987db10d38ec0348c844145e
Mosi <mosisu07@gmail.com>
2024-07-23 01:30:54 +0800

    chore: setting for SPA router

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ module.exports = {
   mode: "development", //有其它模式可以選，非必填
   entry: "./src/index.js", //必須指向 index.js 這個進入點
   output: {
-    path: path.resolve(__dirname, "dist"), //要解析的檔案
+    path: path.resolve(__dirname, "dist"), //指定輸出文件的目錄
     filename: "main.js", //輸出的檔名
+    publicPath: "/", //指定輸出目錄的公共 URL 路徑
   },
   // 任何有新的 Loader 官方都會提供一個 module rules ，就把它加在這裡
   module: {
@@ -42,6 +43,7 @@ module.exports = {
     compress: true,
     port: 9000,
     hot: true, //啟用 HMR
+    historyApiFallback: true, // 啟用 HTML5 History API，有了這個設定才能使用 react-router-dom 切換頁面
   },
   resolve: {
     extensions: [".js", ".jsx"],
