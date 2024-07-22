c81f4d32a0e9c6da5dfa97b354c9aaf8675683d3
Mosi <mosisu07@gmail.com>
2024-07-23 01:48:37 +0800

    chore: added sign up component to router

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: "/home",
     element: <App />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
 ]);
 export default router;
