908f1762fcac65dc37c1c313d3ebf75e603fc4c9
Mosi <mosisu07@gmail.com>
2024-07-22 14:06:39 +0800

    feat: created Login page

---
+++ src/pages/Login/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import api from "../../api.js";
+
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  function handleLogin() {
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    const body = {
+      user: {
+        email: email,
+        password: password,
+      },
+    };
+
+    axios
+      .post(api.login, body, { headers })
+      .then((res) => {
+        res.status === 200 &&
+          localStorage.setItem("token", res.headers.authorization);
+        res.status === 200 && navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  return (
+    <>
+      <h1>Login</h1>
+      <div>
+        <label htmlFor="email">
+          <p>Email</p>
+          <input
+            type="text"
+            id="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          <p>Password</p>
+          <input
+            type="password"
+            id="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleLogin}>
+          登入
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Login;
