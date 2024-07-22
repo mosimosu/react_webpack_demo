7a711f90cb808249d6213f6e2e5baa9b9f5450f0
Mosi <mosisu07@gmail.com>
2024-07-22 14:06:56 +0800

    chore: added Login api

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ const mainUrl = "https://todoo.5xcamp.us/";
 const api = {
   getTodos: `${mainUrl}/todos`,
   postTodos: `${mainUrl}/todos`,
+  login: `${mainUrl}/users/sign_in`,
 };
 
 export default api;
