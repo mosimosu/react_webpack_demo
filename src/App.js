7405d3b19ee95280594908198048aaa60ac7a859
Mosi <mosisu07@gmail.com>
2024-07-23 13:17:45 +0800

    feat: completing feature & radio filter

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import api from "./api.js";
 
 function App() {
   const [data, setData] = useState(null);
+  const [filter, setFilter] = useState("all");
   const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
   const token = localStorage.getItem("token");
+
+  const filteredData =
+    filter === "all"
+      ? data
+      : data.filter((item) => {
+          if (filter === "done") return item.completed_at;
+          if (filter === "undone") return !item.completed_at;
+        });
 
   function handlePost(text) {
     const headers = {
@@ -39,6 +49,38 @@ function App() {
         console.log(err);
       });
   }
+  function handleComplete(id) {
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    };
+    axios
+      .patch(`${api.PATCH_COMPLETE}/${id}/toggle`, {}, { headers })
+      .then((res) => {
+        res.status === 200 &&
+          setData(data.map((item) => (item.id === id ? res.data : item)));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function handleSearch() {
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    };
+    axios
+      .get(`${api.GET_TODOS}?q=${search}`, { headers })
+      .then((res) => {
+        setData(res.data.todos);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   useEffect(() => {
     function fetchData() {
@@ -52,6 +94,7 @@ function App() {
         .get(api.GET_TODOS, { headers })
         .then((res) => {
           setData(res.data.todos);
+          setFilter("all");
         })
         .catch((err) => {
           console.log(err);
@@ -62,7 +105,9 @@ function App() {
 
   return (
     <div>
-      <h1 style={{ color: "red", textAlign: "center" }}>Hello, World!</h1>
+      <h1 style={{ color: "red", textAlign: "center" }}>
+        {`${localStorage.getItem("name")}'s Todo List` || "Todo List"}
+      </h1>
       <div>
         <input
           type="text"
@@ -78,13 +123,69 @@ function App() {
           送出
         </button>
       </div>
+      <div>
+        <input
+          type="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="button" onClick={handleSearch}>
+          搜尋
+        </button>
+      </div>
+      <div>
+        <label htmlFor="all">
+          <input
+            type="radio"
+            name="itemStatus"
+            id="all"
+            value={"all"}
+            checked={filter === "all"}
+            onChange={() => setFilter("all")}
+          />
+          all
+        </label>
+        <label htmlFor="done">
+          <input
+            type="radio"
+            name="itemStatus"
+            id="done"
+            value={"done"}
+            checked={filter === "done"}
+            onChange={() => setFilter("done")}
+          />
+          done
+        </label>
+        <label htmlFor="undone">
+          <input
+            type="radio"
+            name="itemStatus"
+            id="undone"
+            value={"undone"}
+            checked={filter === "undone"}
+            onChange={() => setFilter("undone")}
+          />
+          undone
+        </label>
+      </div>
       <ul>
-        {data &&
-          data?.map((item) => {
+        {filteredData &&
+          filteredData.map((item) => {
             return (
-              <li key={item.id} style={{ display: "flex" }}>
+              <li key={item.id} style={{ display: "flex", gap: "10px" }}>
                 <p>{item.content}</p>
-                <button type="button" onClick={() => handleDelete(item.id)}>
+                <button
+                  type="button"
+                  onClick={() => handleComplete(item.id)}
+                  style={{ padding: "2px" }}
+                >
+                  {item.completed_at ? "已完成" : "未完成"}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleDelete(item.id)}
+                  style={{ padding: "2px" }}
+                >
                   刪除
                 </button>
               </li>
