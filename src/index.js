019efbb5055adf44c82d78e9d689b969f72a6c0c
Mosi <mosisu07@gmail.com>
2024-07-31 15:38:21 +0800

    chore: set QueryClient, QueryClientProvider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "styled-components";
 import theme from "./constants/theme";
 import GlobalStyle from "./constants/globalStyle";
 import router from "./router/router";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
